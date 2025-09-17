334f27b1edd13efad954d8032b7750db15c8addf
DESKTOP-QTVOPS7\Користувач <ge65quk@mytum.de>
2025-09-17 18:18:28 +0200

    adapted the gcode analysis loading

--- gcode-analysis-service/app.py
+++ gcode-analysis-service/app.py
@@ -137,4 +137,4 @@ def analyze_gcode_endpoint():
     return jsonify(response_data), 200
 
 if __name__ == "__main__":
-    app.run(debug=True, use_reloader=False, host="0.0.0.0", port=5005)
+    app.run(debug=True, host="0.0.0.0", port=5005)
