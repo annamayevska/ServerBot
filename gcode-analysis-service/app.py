5256cc70ff7d711cd83a32005c1e88af860e0171
DESKTOP-QTVOPS7\Користувач <ge65quk@mytum.de>
2025-09-26 19:54:00 +0200

    adjusted endpoint

--- gcode-analysis-service/app.py
+++ gcode-analysis-service/app.py
@@ -138,7 +138,7 @@ def compute_features(parsed):
     }
 
 # --- Flask endpoint ---
-@app.route('/', methods=['POST'])
+@app.route('/analyzeGcode', methods=['POST'])
 def analyze_gcode_endpoint():
     gcode = None
     if request.content_type == 'application/x-www-form-urlencoded':
